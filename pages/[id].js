eeaa439c35b45346aaf0be823b3f5f01755f69b9
Raj Shankar Tiwary <rajshankartiwary@gmail.com>
2022-12-24 10:43:47 +0530

    favicon, meta and title update

--- pages/[id].js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SavedPitch = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default SavedPitch;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import { useContext } from "react";
-import PitchText from "../components/PitchText";
-import { PromptContext } from "../context/PromptContext";
+import Head from "next/head";
 import { QueryClientProvider, QueryClient } from "react-query"
 import Main from "../components/Main";
 import Container from "../components/wrapper/Container";
@@ -9,9 +7,15 @@ export default function Home() {
   const queryClient = new QueryClient();
 
   return (
-      <Container className="p-6 md:p-10 overflow-hidden flex flex-col lg:flex-row min-h-full w-screen ">
-          <Main />
-      </Container>
+      <>
+        <Head>
+          <title>PitchMe by Ravoluzen</title>
+          <meta name='description' content='Generate referral pitches and cover lettrs using AI, edit and save them.' />
+        </Head>
+        <Container className="p-6 md:p-10 overflow-hidden flex flex-col lg:flex-row min-h-full w-screen ">
+            <Main />
+        </Container>
+      </>
 
   )
 }
